81ab3079ae02f6bd37458c6ed15fe9137ad90e03
MarvanT <marvan.t98@gmail.com>
2023-10-24 14:26:25 +0100

    Refactor IndustryIdentifier fields in Book model
    The IndustryIdentifierISBN13 and IndustryIdentifierISBN10 fields were removed from the Book class and replaced with a list of IndustryIdentifier objects. The change was made in order to better organize industry identifier information and improve scalability.

--- BestReads/Core/Model/Book.cs
+++ BestReads/Core/Model/Book.cs
@@ -6,8 +6,8 @@ namespace BestReads.Core;
 public class Book
 {
     [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)] 
-    public string Id { get; set; } 
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
     public string Title { get; set; }
     public List<string> Authors { get; set; } = new();
     public List<string> Categories { get; set; } = new();
@@ -16,8 +16,6 @@ public class Book
     public DateTime PublishedDate { get; set; }
     public string Thumbnail { get; set; }
     public string GoogleBooksId { get; set; }
-    public string? IndustryIdentifierISBN13 { get; set; }
-    public string? IndustryIdentifierISBN10 { get; set; }
+    public List<IndustryIdentifier> IndustryIdentifiers { get; set; } = new();
     public float[] Embeddings { get; set; } = Array.Empty<float>();
-    //Todo: update industry identifier to be in a collection
 }
