f218bb4f9e0fd6706940911f5e5b847941980854
MarvanT <marvan.t98@gmail.com>
2023-10-26 16:55:35 +0100

    Allow null values in book and database settings properties
    The properties in the MongoDbSettings.cs and GetBookRecommendationsDto.cs class have been updated to be nullable. This has been done to handle situations where there may not be any data available for these fields when reading from the database or loading book information. This modification prevents possible null reference issues beforehand.

--- BestReads/Features/BookRecommendations/Dtos/GetBookRecommendationsDto.cs
+++ BestReads/Features/BookRecommendations/Dtos/GetBookRecommendationsDto.cs
@@ -4,13 +4,13 @@ namespace BestReads.Features.BookRecommendations.Dtos;
 
 public class GetBookRecommendationsDto
 {
-    public string Title { get; set; }
-    public List<string> Authors { get; set; }
-    public List<string> Categories { get; set; }
-    public string Description { get; set; }
-    public string Publisher { get; set; }
-    public DateTime PublishedDate { get; set; }
-    public string Thumbnail { get; set; }
-    public string GoogleBooksId { get; set; }
-    public List<IndustryIdentifier> IndustryIdentifiers { get; set; }
+    public string? Title { get; set; }
+    public List<string> Authors { get; set; } = new();
+    public List<string> Categories { get; set; } = new();
+    public string? Description { get; set; }
+    public string? Publisher { get; set; }
+    public DateTime? PublishedDate { get; set; }
+    public string? Thumbnail { get; set; }
+    public string? GoogleBooksId { get; set; }
+    public List<IndustryIdentifier> IndustryIdentifiers { get; set; } = new();
 }
