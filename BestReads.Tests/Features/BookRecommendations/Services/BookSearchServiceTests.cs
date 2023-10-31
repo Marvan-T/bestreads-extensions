54fb8d9fc18688ba4233bd5303d16f6418349c3f
MarvanT <marvan.t98@gmail.com>
2023-10-31 20:19:47 +0000

    "Add unit tests for Book Search and Recommendation services
    Created new unit tests for the near-book search service. Modified the BookRecommendationServiceTests to incorporate new method calls related to the addition of the BookSearchService. Checks added for success path, fetch of embeddings and return of recommendations were included."

---
+++ BestReads.Tests/Features/BookRecommendations/Services/BookSearchServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using BestReads.Features.BookRecommendations.Dtos;
+using BestReads.Features.BookRecommendations.Services.BookSearchService;
+using BestReads.Infrastructure.AzureSearchClient;
+using BestReads.Tests.Fakers;
+
+namespace BestReads.Tests.Features.BookRecommendations.Services;
+
+public class BookSearchServiceTests
+{
+    private readonly Mock<IAzureSearchClient> _azureSearchClientMock;
+    private readonly BookSearchService _bookSearchService;
+
+    public BookSearchServiceTests()
+    {
+        _azureSearchClientMock = new Mock<IAzureSearchClient>();
+        _bookSearchService = new BookSearchService(_azureSearchClientMock.Object);
+    }
+
+    [Fact]
+    public async Task GetNearestNeighbors_WhenCalled_ReturnsExpectedRecommendations()
+    {
+        // Arrange
+        var book = BookFakers.BookModelFaker().Generate();
+        var expectedRecommendations = BookFakers.BookRecommendationDtoFaker().Generate(5);
+
+        _azureSearchClientMock
+            .Setup(x => x.SingleVectorSearch<BookRecommendationDto>(
+                It.IsAny<float[]>(),
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<List<string>>()))
+            .ReturnsAsync(expectedRecommendations);
+
+        // Act
+        var result = await _bookSearchService.GetNearestNeighbors(book);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().BeEquivalentTo(expectedRecommendations);
+
+        var expectedFilter = $"GoogleBooksId ne '{book.GoogleBooksId}'";
+        var expectedSelectOptions = new List<string>
+        {
+            "doc_id, GoogleBooksId, Title, Authors, Categories, Description, Publisher, PublishedDate, Thumbnail, IndustryIdentifiers"
+        };
+        _azureSearchClientMock.Verify(x => x.SingleVectorSearch<BookRecommendationDto>(
+            book.Embeddings,
+            "Embeddings",
+            expectedFilter,
+            expectedSelectOptions), Times.Once);
+    }
+}
