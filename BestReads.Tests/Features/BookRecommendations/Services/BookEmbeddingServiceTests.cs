5f94874e077c01bad867be7d1beba1996e53df0d
MarvanT <marvan.t98@gmail.com>
2023-11-21 16:29:18 +0000

    Add validation tests for ConstructEmbeddingRequest
    This commit adds a series of tests for the ConstructEmbeddingRequest function in the BookEmbeddingServiceTests. It verifies that the function responds appropriately when provided with invalid titles, authors, categories, and descriptions. This will ensure robust error handling and accurate identification of invalid inputs in our book recommendation feature.

--- BestReads.Tests/Features/BookRecommendations/Services/BookEmbeddingServiceTests.cs
+++ BestReads.Tests/Features/BookRecommendations/Services/BookEmbeddingServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using BestReads.Core;
 using BestReads.Features.BookRecommendations.Dtos;
+using BestReads.Features.BookRecommendations.Errors;
 using BestReads.Features.BookRecommendations.Services.BookEmbeddingService;
 using BestReads.Tests.Fakers;
 
@@ -26,11 +27,12 @@ public class BookEmbeddingServiceTests
         var result = _bookEmbeddingService.ConstructEmbeddingRequest(fakeBookDto);
 
         // Assert
+        result.IsSuccess.Should().BeTrue();
         var expectedText = $"Title: {fakeBookDto.Title};" +
                            $"Authors: {string.Join(", ", fakeBookDto.Authors)};" +
                            $"Categories: {string.Join(", ", fakeBookDto.Categories)};" +
                            $"Description: {fakeBookDto.Description};";
-        result.Text.Should().Be(expectedText);
+        result.Data.Text.Should().Be(expectedText);
     }
 
     [Fact]
@@ -48,4 +50,82 @@ public class BookEmbeddingServiceTests
         result.Should().Equal(expectedEmbeddings);
         _openAiClientMock.Verify(x => x.GetEmbeddingsAsync(fakeRequest), Times.Once);
     }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void ConstructEmbeddingRequest_Should_Return_Failure_When_Title_Is_Invalid(string title)
+    {
+        // Arrange
+        var fakeBookDto = BookFakers.GetBookRecommendationDtoFaker().Generate();
+        fakeBookDto.Title = title;
+
+        // Act
+        var result = _bookEmbeddingService.ConstructEmbeddingRequest(fakeBookDto);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Be(GenerateRecommendationErrors.TitleRequired);
+        _openAiClientMock.Verify(x => x.GetEmbeddingsAsync(It.IsAny<EmbeddingRequest>()), Times.Never);
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidListData))]
+    public void ConstructEmbeddingRequest_Should_Return_Failure_When_Authors_Are_Invalid(List<string> authors)
+    {
+        // Arrange
+        var fakeBookDto = BookFakers.GetBookRecommendationDtoFaker().Generate();
+        fakeBookDto.Authors = authors;
+
+        // Act
+        var result = _bookEmbeddingService.ConstructEmbeddingRequest(fakeBookDto);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Be(GenerateRecommendationErrors.AuthorsRequired);
+        _openAiClientMock.Verify(x => x.GetEmbeddingsAsync(It.IsAny<EmbeddingRequest>()), Times.Never);
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidListData))]
+    public void ConstructEmbeddingRequest_Should_Return_Failure_When_Categories_Are_Invalid(List<string> categories)
+    {
+        // Arrange
+        var fakeBookDto = BookFakers.GetBookRecommendationDtoFaker().Generate();
+        fakeBookDto.Categories = categories;
+
+        // Act
+        var result = _bookEmbeddingService.ConstructEmbeddingRequest(fakeBookDto);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Be(GenerateRecommendationErrors.CategoriesRequired);
+        _openAiClientMock.Verify(x => x.GetEmbeddingsAsync(It.IsAny<EmbeddingRequest>()), Times.Never);
+    }
+
+    public static IEnumerable<object[]> InvalidListData()
+    {
+        yield return new object[] { new List<string>() }; // Empty list
+        yield return new object[] { null }; // Null list
+        yield return new object[] { new List<string> { "", " " } }; // List with empty or whitespace strings
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void ConstructEmbeddingRequest_Should_Return_Failure_When_Description_Is_Invalid(string description)
+    {
+        // Arrange
+        var fakeBookDto = BookFakers.GetBookRecommendationDtoFaker().Generate();
+        fakeBookDto.Description = description;
+
+        // Act
+        var result = _bookEmbeddingService.ConstructEmbeddingRequest(fakeBookDto);
+
+        // Assert
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Be(GenerateRecommendationErrors.DescriptionRequired);
+        _openAiClientMock.Verify(x => x.GetEmbeddingsAsync(It.IsAny<EmbeddingRequest>()), Times.Never);
+    }
+
 }
