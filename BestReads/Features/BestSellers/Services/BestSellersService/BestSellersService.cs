f1812c4f79d965d1605ce2f07f4a6ba6197f4fbc
MarvanT <marvan.t98@gmail.com>
2024-06-08 17:30:10 +0100

    feat: Refactor BestSellersService to remove unnecessary logging
    The BestSellersService class in the BestSellersService.cs file has been refactored to remove unnecessary logging. The ILogger<BestSellersService> bestSellerServiceLogger parameter has been removed from the constructor and the corresponding logging code has been deleted. This change simplifies the code and improves performance.

--- BestReads/Features/BestSellers/Services/BestSellersService/BestSellersService.cs
+++ BestReads/Features/BestSellers/Services/BestSellersService/BestSellersService.cs
@@ -5,24 +5,14 @@ using BestReads.Infrastructure.ApiClients.NYTimes.DTOs;
 
 namespace BestReads.Features.BestSellers.Services.BestSellersService;
 
-public class BestSellersService(INYTimesApiClient nyTimesApiClient, ILogger<BestSellersService> bestSellerServiceLogger)
+public class BestSellersService(INYTimesApiClient nyTimesApiClient)
     : IBestSellersService
 {
-    private readonly ILogger<BestSellersService> _bestSellerServiceLogger = bestSellerServiceLogger;
     private readonly INYTimesApiClient _nyTimesApiClient = nyTimesApiClient;
 
     public async Task<Result<BestSellerListDto>> GetBestSellersAsync()
     {
-        try
-        {
-            var bestSellers = await _nyTimesApiClient.GetCurrentBestSellersList();
-            return Result<BestSellerListDto>.Success(bestSellers);
-        }
-        catch (Exception e)
-        {
-            // log the exception errors to a third party service when it's implemented 
-            _bestSellerServiceLogger.LogError(e.Message);
-            return Result<BestSellerListDto>.Failure(BestSellerErrors.FailedToRetrieveBestSellersList);
-        }
+        var bestSellers = await _nyTimesApiClient.GetCurrentBestSellersList();
+        return Result<BestSellerListDto>.Success(bestSellers); 
     }
 }
