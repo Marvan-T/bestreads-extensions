862f87d7a132cd42f950cadfefb1b0e9f43ff78c
MarvanT <marvan.t98@gmail.com>
2024-06-28 20:02:03 +0100

    Add unit test for BestSellersService

---
+++ BestReads.Tests/Features/BestSellers/Services/BestSellersServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using BestReads.Features.BestSellers.Services.BestSellersService;
+using BestReads.Infrastructure.ApiClients.NYTimes;
+using BestReads.Infrastructure.ApiClients.NYTimes.DTOs;
+
+namespace BestReads.Tests;
+
+public class BestSellersServiceTests
+{
+    private readonly Mock<INYTimesApiClient> _mockNyTimesApiClient;
+    private readonly BestSellersService _bestSellersService;
+
+    public BestSellersServiceTests()
+    {
+        _mockNyTimesApiClient = new Mock<INYTimesApiClient>();
+        _bestSellersService = new BestSellersService(_mockNyTimesApiClient.Object);
+    }
+
+    [Fact]
+    public async Task GetBestSellersAsync_Success_ReturnsBestSellerListDto()
+    {
+        // Arrange
+        var expectedBestSellerListDto = new BestSellerListDto();
+        _mockNyTimesApiClient
+            .Setup(x => x.GetCurrentBestSellersList())
+            .ReturnsAsync(expectedBestSellerListDto);
+
+        // Act
+        var result = await _bestSellersService.GetBestSellersAsync();
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Data.Should().BeEquivalentTo(expectedBestSellerListDto);
+    }
+}
