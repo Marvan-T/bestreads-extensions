9f913180e81068914dccc79da7fa71fb6af0496f
Marvan-T <65969444+Marvan-T@users.noreply.github.com>
2024-07-28 19:47:44 +0000

    Fixing the log configuration to write to console and files in dev mode

--- BestReads/TestController.cs
+++ BestReads/TestController.cs
@@ -7,6 +7,8 @@ public class TestController(ILogger<TestController> logger) : ControllerBase
     [HttpGet]
     public void TestLogging()
     {
+       var book = new { Author = "Hectar Gacia and Francesg Miralles", Title = "Ikigai"};
         logger.LogInformation("Hello from Serilog");
+        logger.LogInformation("Process the book {@book}", book);
     }
 }
